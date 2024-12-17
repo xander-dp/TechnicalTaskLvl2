1d5d55883dfe61fc888bdba7e5c849a6c246487a
xander_savchenko <oleksandr.savchenko@innowise.com>
2024-12-17 13:52:10 +0100

    Applied project to requirements
    swift compiler version set to 5 (5.10)
    deployment target set to iOS 15
    deleted generated code/comments
    default project files restructured

---
+++ ShipsInfo/Application/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  ShipsInfo
+//
+//  Created by Oleksandr Savchenko on 17.12.24.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        true
+    }
+}
+
